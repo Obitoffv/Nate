02a6eb8cefa6eba99c185a05a02db829c96256e1
YUN神 <110736685+Alonebots@users.noreply.github.com>
2022-10-01 05:35:13 -0800

    Create data.py

---
+++ MIKU/data.py
@@ -0,0 +1,16 @@
+RAID = [
+    "Hello",
+]
+
+DEADLYSPAM = [5001899507]
+
+REPLYRAID = [
+    "Hello",
+]
+
+GROUP = [-1001299748978]
+
+
+PORMS = [
+        "https://telegra.ph/file/3b69c00e101b4df7ff3f9.jpg",
+]
